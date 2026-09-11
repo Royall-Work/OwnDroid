b108ba9aaac087b55681d16577f78f0463f6d8f3
Royall-Work <araafroyall.dual@gmail.com>
2026-09-11 09:38:11 +0530

    Comment out delete work profile option

--- app/src/main/java/com/bintianqi/owndroid/feature/work_profile/WorkProfileScreen.kt
+++ app/src/main/java/com/bintianqi/owndroid/feature/work_profile/WorkProfileScreen.kt
@@ -56,6 +56,11 @@ fun WorkProfileScreen(
         FunctionItem(R.string.intent_filter, icon = R.drawable.filter_alt_fill0) {
             onNavigate(Destination.CrossProfileIntentFilter)
         }
+        /*
+        FunctionItem(R.string.delete_work_profile, icon = R.drawable.delete_forever_fill0) {
+            onNavigate(Destination.DeleteWorkProfile)
+        }
+        */
     }
 }
 
