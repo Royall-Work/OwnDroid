2b1f04cdf2ff306817a4af4280c82a3e105f466b
Royall-Work <araafroyall.dual@gmail.com>
2026-09-11 05:47:25 +0530

    Update package identity references

--- app/src/main/java/com/bintianqi/owndroid/utils/Utils.kt
+++ app/src/main/java/com/bintianqi/owndroid/utils/Utils.kt
@@ -1,4 +1,4 @@
-package com.bintianqi.owndroid.utils
+package com.bintianqi.owndroid
 
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
@@ -118,8 +118,7 @@ fun String.hash(): String {
     return md.digest(this.encodeToByteArray()).toHexString()
 }
 
-val MyAdminComponent = ComponentName.unflattenFromString("com.bintianqi.owndroid/.Receiver")!!
-
+val MyAdminComponent = ComponentName.unflattenFromString("com.royall.owndroid/.Receiver")!!
 
 @OptIn(ExperimentalStdlibApi::class)
 fun getPackageSignature(info: PackageInfo): String? {
@@ -189,49 +188,14 @@ val getInstalledAppsFlags =
 fun searchInString(query: String, content: String)
         = query.split(' ').all { content.contains(it, true) }
 
-class ToastChannel(val context: Context) {
-    val channel = Channel<String>(0, BufferOverflow.DROP_LATEST)
-    fun sendStatus(status: Boolean) {
-        val resId = if (status) R.string.success else R.string.failed
-        channel.trySend(context.getString(resId))
-    }
-    fun sendText(text: String) {
-        channel.trySend(text)
-    }
-    fun sendText(resId: Int) {
-        channel.trySend(context.getString(resId))
-    }
-}
-
-class AppInfo(
-    val name: String,
-    val label: String,
-    val icon: Drawable,
-    val flags: Int
-) {
-    val isSystem get() = (flags and ApplicationInfo.FLAG_SYSTEM) != 0
-}
-
-fun getAppInfo(pm: PackageManager, info: ApplicationInfo) =
-    AppInfo(info.packageName, info.loadLabel(pm).toString(), info.loadIcon(pm), info.flags)
-
-fun getAppInfo(pm: PackageManager, name: String): AppInfo {
-    return try {
-        getAppInfo(pm, pm.getApplicationInfo(name, getInstalledAppsFlags))
-    } catch (_: PackageManager.NameNotFoundException) {
-        AppInfo(name, "???", Color.Transparent.toArgb().toDrawable(), 0)
-    }
-}
-
-fun <T>List<T>.plusOrMinus(state: Boolean, item: T) = if (state) plus(item) else minus(item)
-fun <T>List<T>.plusOrMinus(state: Boolean, items: Collection<T>) =
-    if (state) plus(items) else minus(items)
+val String.isValidPackageName
+    get() = Regex("""^(?:[a-zA-Z]\w*\.)+[a-zA-Z]\w*$""").matches(this)
 
 fun uninstallPackage(
     application: MyApplication, privilegeHelper: PrivilegeHelper,
     packageName: String, onComplete: (String?) -> Unit
 ) {
-    val action = "com.bintianqi.owndroid.action.PACKAGE_UNINSTALLED"
+    val action = "com.royall.owndroid.action.PACKAGE_UNINSTALLED"
     val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             val statusExtra = intent.getIntExtra(PackageInstaller.EXTRA_STATUS, 999)
@@ -266,12 +230,13 @@ fun uninstallPackage(
     application.getPackageInstaller(privilegeHelper.dhizuku).uninstall(packageName, pi)
 }
 
+fun <T>List<T>.plusOrMinus(state: Boolean, item: T) = if (state) plus(item) else minus(item)
+fun <T>List<T>.plusOrMinus(state: Boolean, items: Collection<T>) =
+    if (state) plus(items) else minus(items)
+
 fun viewModelFactory(build: () -> ViewModel) =
     object : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
             return build() as T
         }
     }
-
-val String.isValidPackageName
-    get() = Regex("""^(?:[a-zA-Z]\w*\.)+[a-zA-Z]\w*$""").matches(this)
