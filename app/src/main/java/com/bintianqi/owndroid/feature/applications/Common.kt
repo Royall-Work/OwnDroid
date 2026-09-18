0afe27ba8a84b4c08504fce1496c1481adf7f5f4
Royall-Work <araafroyall.dual@gmail.com>
2026-09-18 10:27:18 +0530

    Guard unsupported delegated app status

--- app/src/main/java/com/bintianqi/owndroid/feature/applications/Common.kt
+++ app/src/main/java/com/bintianqi/owndroid/feature/applications/Common.kt
@@ -333,10 +333,10 @@ fun getAppStatus(
             }
             hidden = dpm.isApplicationHidden(admin, packageName)
             ub = dpm.isUninstallBlocked(admin, packageName)
-            if (Build.VERSION.SDK_INT >= 30) {
-                ucd = packageName in dpm.getUserControlDisabledPackages(admin)
+            if (Build.VERSION.SDK_INT >= 30 && !ph.delegatedAdmin) {
+                ucd = packageName in dpm.getUserControlDisabledPackages(dar)
             }
-            if (Build.VERSION.SDK_INT >= 28) {
+            if (Build.VERSION.SDK_INT >= 28 && !ph.delegatedAdmin) {
                 mdd = packageName in dpm.getMeteredDataDisabledPackages(dar)
             }
         }
